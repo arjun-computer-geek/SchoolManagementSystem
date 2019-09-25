8897ebc0915fbe50d4c767cb16cbd4834148a9c1
arjun <arjun@localhost.localdomain>
2019-09-26 01:58:40 +0530

    modifing website

--- index.js
+++ index.js
@@ -7,6 +7,30 @@ app.set('view engine','pug')
 app.set('views', 'views')
 
 app.get('/', (req, res) =>{
-res.render('home')
+    res.render('home')
+})
+
+app.get('/admission', (req, res) =>{
+    res.render('admission')
+})
+
+app.get('/student_login', (req, res) =>{
+    res.render('login')
+})
+
+app.get('/parent_login', (req, res) =>{
+    res.render('login')
+})
+
+app.get('/teacher_login', (req, res) =>{
+    res.render('login')
+})
+
+app.get('/librarian_login', (req, res) =>{
+    res.render('login')
+})
+
+app.get('/admin_login', (req, res) =>{
+    res.render('login')
 })
 app.listen(8000)
